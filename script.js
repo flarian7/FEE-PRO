68b6db3ab27d9eae07f092ea309152a7c4f7f3c9
flarian7 <138358543+flarian7@users.noreply.github.com>
2024-05-08 11:50:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,178 @@
+// let n = prompt("enter the no");
+// n = parseInt(n)
+// switch(n)
+// {
+//     case 1:
+//     console.log("Monday");
+//     break;
+//     case 2:
+//     console.log("Tuesday");
+//     break;
+//     case 3:
+//     console.log("Wednesday");
+//     break;
+//     case 4:
+//     console.log("Thursday");
+//     break;
+//     case 5:
+//     console.log("Friday");
+//     break;
+//     case 6:
+//     console.log("Saturday");
+//     break;
+//     case 7:
+//     console.log("Sunday");
+//     break;
+//     default:
+//     console.log("Error");
+
+
+// }
+
+
+
+
+// Q1 Assignment Q ///
+
+// let num = prompt("enter the number");
+// num = parseInt(num);
+
+// if(num%10==0)
+// {
+//   console.log("good");
+// }
+
+// else
+// {
+//     console.log("Bad");
+// }
+
+// Q-2 Assignment Q ///
+
+// let age = prompt("enter the age");
+// let name = prompt("enetr the name");
+// age = parseInt(age);
+
+// console.log(`${name} is ${age} yrs old`);
+
+// Q-3 Assignment Q ///
+
+// let quarter = prompt("enter the quarter number");
+// quarter = parseInt(quarter);
+
+// switch(quarter) {
+//     case 1 :
+//     console.log("January, feburary, March");
+//     break;
+//     case 2 :
+//     console.log("April, May, June");
+//     break;
+//     case 3 :
+//     console.log("July, August, September");
+//     break;
+//     case 4 :
+//     console.log("October, November, December");
+//     break;
+//     default :
+//     console.log("Error");
+
+
+
+// }
+
+// Q-4 Assignment Q ///
+
+// let str = prompt("enter the character");
+
+// if(((str[0]==="a") ||(str[0]=== "A")) && ((str.length>5)))
+// {
+//     console.log("golden string");
+// }
+
+// else 
+// {
+//     console.log("not a golden string");
+// }
+
+// Q-5 Assignment Q ///
+
+// let a = prompt("enter the number1");
+// let b = prompt("enter the number2");
+// let c = prompt("enter the number3");
+// a = parseInt(a);
+// b = parseInt(b);
+// c = parseInt(c);
+
+// if(a>b)
+// {
+//     if(b>c)
+//     {
+//         console.log("a is the greatest");
+//     }
+
+//     else
+//     {
+//         console.log("c is the greatest");
+//     }
+
+// }
+
+// else 
+// {
+//     if(a>c)
+//     {
+//         console.log("b is the greatest");
+//     }
+
+//     else 
+//     {
+//         console.log("c is the greatest");
+//     }
+// }
+
+//  Q-5 Assignment Q ///
+
+// let a = prompt("enetr the number 1");
+// let b = prompt("enetr the number 2");
+//  a = parseInt(a);
+//  b = parseInt(b);
+
+// if((a%10) == (b % 10))
+// {
+//     console.log("a and b both have the same last digit");
+// }
+
+// else{
+//     console.log("they dont have last digit same");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
